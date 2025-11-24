6933d208a8bd6d8f8eba72775ca0051e889a4551
Thiago <tihsouza01@gmail.com>
2025-11-24 14:51:20 -0300

    Refact: ajusta controle de erro e organiza seções do código para melhor legibilidade

--- Ex-Arquivos-Sequenciais/arqseq.cob
+++ Ex-Arquivos-Sequenciais/arqseq.cob
@@ -12,7 +12,8 @@
        FILE-CONTROL.
            SELECT ARQIN ASSIGN TO
                "C:\Users\WIN 11\OneDrive\Desktop\Cob-Arq\ARQIN.txt"
-           ORGANIZATION IS LINE SEQUENTIAL.
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS WS-STATUS-IN.
       *
        DATA                    DIVISION.
       *
@@ -21,13 +22,19 @@
        01  ARQ-REC             PIC X(21).
       *
        WORKING-STORAGE         SECTION.
+      *  ORGANIZACAO DO ARQUIVO 
        01  WS-ARQIN.
            05 WS-ARQ-COD          PIC 9(04) VALUE ZEROS.
            05 WS-ARQ-NAME         PIC X(15) VALUE SPACES.
            05 WS-ARQ-AGE          PIC 99    VALUE ZEROS.
-
+           
+      * CONTROLE DOS ARQUIVOS     
        77  EOF-ARQ                PIC A     VALUE 'N'.
-       77  WS-STATUS-IN           PIC XX.
+       77  WS-STATUS-IN           PIC XX. 
+       
+      * CONTROLE DE ERROS 
+       77  WS-ABEND-CODE          PIC XX    VALUE SPACES.
+       77  WS-ABEND-MESSAGE       PIC X(30) VALUE SPACES.
       *
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -35,13 +42,16 @@
            PERFORM UNTIL EOF-ARQ EQUAL 'Y'
                PERFORM 200-PROCESS-FILE
            END-PERFORM.
-           PERFORM 900-CLOSE-FILE.
+           PERFORM 999-CLOSE-FILE.
            STOP RUN.
                
        100-OPEN-FILE.    
            OPEN INPUT ARQIN.
-           IF WS-STATUS-IN EQUAL '00'
-               CONTINUE
+           IF WS-STATUS-IN NOT EQUAL '00'
+               MOVE WS-STATUS-IN           TO WS-ABEND-CODE
+               MOVE "FILE ARQIN IO/ERROR"  TO WS-ABEND-MESSAGE
+               PERFORM 900-ABEND THRU 999-CLOSE-FILE
+               STOP RUN
            END-IF.
                
        200-PROCESS-FILE.
@@ -55,7 +65,12 @@
                    DISPLAY '----------------------'
            END-READ.
 
-       900-CLOSE-FILE.
+       900-ABEND                  SECTION.
+           DISPLAY "***************************************************"
+           DISPLAY "****************  ABEND ROUTINE  ******************"
+           DISPLAY "* ABEND CODE: " WS-ABEND-CODE.
+           
+       999-CLOSE-FILE.
             CLOSE ARQIN.
             
        END PROGRAM ARQSEQ.
